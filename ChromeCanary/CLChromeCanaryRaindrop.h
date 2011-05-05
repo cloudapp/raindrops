de5a8c162a6c60ae16dd24f68196a934f5d70e0d
John Michel <cowbird@gmail.com>
2011-05-04 22:12:31 -0400

    Adding Chrome Canary build raindrop

---
+++ ChromeCanary/CLChromeCanaryRaindrop.h
@@ -0,0 +1,20 @@
+//
+//  CLChromeCanaryRaindrop.h
+//  Chrome
+//
+//  Created by Matthias Plappert on 20.02.11.
+//  Copyright 2011 phaps. All rights reserved.
+//
+//  Modified by John Michel on 05042011
+//  Copyright 2011 John Michel. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "CLRaindropProtocol.h"
+
+
+@interface CLChromeCanaryRaindrop : NSObject <CLRaindropProtocol> {
+
+}
+
+@end
