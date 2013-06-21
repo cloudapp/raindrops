66da45d88bbd28091d8c60b59572b97d1cd4a8c7
Zach Waugh <zwaugh@gmail.com>
2013-06-21 17:33:27 -0400

    Remove bundle entitlements. Add some logging to see where it's failing

--- Chrome/CLChromeRaindrop.m
+++ Chrome/CLChromeRaindrop.m
@@ -14,8 +14,9 @@
 
 - (NSString *)pasteboardNameForTriggeredRaindrop
 {
-    ChromeApplication *chrome = [SBApplication applicationWithBundleIdentifier:@"com.google.Chrome"];
+  ChromeApplication *chrome = [SBApplication applicationWithBundleIdentifier:@"com.google.Chrome"];
 	if (chrome == nil) {
+    NSLog(@"[raindrop:chrome] error creating application");
 		return nil;
 	}
 	
@@ -23,10 +24,14 @@
 	ChromeWindow *frontmostWindow = nil;
 	SBElementArray *windows = [chrome windows];
 	if (windows == nil) {
+    NSLog(@"[raindrop:chrome] error getting windows");
 		return nil;
 	}
+  
+  NSLog(@"[raindrop:chrome] chrome windows: %@ - %d", windows, windows.count);
 	
 	for (ChromeWindow *window in windows) {
+    NSLog(@"[raindrop:chrome] window: %@", window);
 		if (frontmostWindow == nil) {
 			frontmostWindow = window;
 		} else if (frontmostWindow.index > window.index) {
@@ -35,15 +40,19 @@
 	}
 	
 	if (frontmostWindow == nil) {
+    NSLog(@"[raindrop:chrome] error, no frontmost window");
 		return nil;
 	}
 	
 	// Get active tab
 	ChromeTab *tab = [frontmostWindow activeTab];
 	if (tab == nil || tab.URL == nil) {
+    NSLog(@"[raindrop:chrome] error getting active tab");
 		return nil;
 	}
 	
+  NSLog(@"[raindrop:chrome] using tab: %@ - %@", tab.title, tab.URL);
+  
 	// Write to pasteboard
 	NSPasteboardItem *item = [[NSPasteboardItem alloc] init];
 	[item setString:tab.title != nil ? tab.title : tab.URL forType:@"public.url-name"];
--- Photoshop/CLPhotoshopRaindrop.m
+++ Photoshop/CLPhotoshopRaindrop.m
@@ -19,29 +19,31 @@
 	PhotoshopDocument *currDoc = [adobePhotoshop currentDocument];	
 	PhotoshopDocument *temp = [currDoc saveIn:[NSURL URLWithString:kTempFileLocation] as:PhotoshopSvFmPNG copying:YES appending:PhotoshopE300LowercaseExtension withOptions:nil];
  
-	if (temp == nil)
-		return nil;
+	if (temp == nil) return nil;
 	
 	NSString *name = [currDoc name];
 	name = [name stringByDeletingPathExtension];
-	if (name == nil || [name length] == 0)
-		name = @"Photoshop Document";
+	if (name == nil || name.length == 0) {
+    name = @"Photoshop Document";
+  }
+		
 	
 	name = [name stringByAppendingString:@".png"];
 
 	NSData *imageData = [NSData dataWithContentsOfFile:kTempFileLocation];
 	
-	if ([[NSFileManager defaultManager] fileExistsAtPath:kTempFileLocation]) //Sanity check
+	if ([[NSFileManager defaultManager] fileExistsAtPath:kTempFileLocation]) {
+    //Sanity check
 		[[NSFileManager defaultManager] removeItemAtPath:kTempFileLocation error:nil];
+  }
 
-	if (imageData == nil)
-		return nil;
+	if (imageData == nil) return nil;
 	
 	NSPasteboard *pasteboard = [NSPasteboard pasteboardWithUniqueName];
 	NSPasteboardItem *item = [[[NSPasteboardItem alloc] init] autorelease];
 	[item setData:imageData forType:(NSString *)kUTTypePNG];
 	[item setString:name forType:@"public.url-name"];
-	[pasteboard writeObjects:[NSArray arrayWithObject:item]];
+	[pasteboard writeObjects:@[item]];
 
 	return [pasteboard name];
 }
