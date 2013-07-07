3616cabc6bf644b2897b8b33990ca15d516beb28
Zach Waugh <zwaugh@gmail.com>
2013-07-07 18:21:40 -0400

    Remove logging. Update project settings

--- Chrome/CLChromeRaindrop.m
+++ Chrome/CLChromeRaindrop.m
@@ -9,14 +9,12 @@
 #import "CLChromeRaindrop.h"
 #import "Chrome.h"
 
-
 @implementation CLChromeRaindrop
 
 - (NSString *)pasteboardNameForTriggeredRaindrop
 {
   ChromeApplication *chrome = [SBApplication applicationWithBundleIdentifier:@"com.google.Chrome"];
 	if (chrome == nil) {
-    NSLog(@"[raindrop:chrome] error creating application");
 		return nil;
 	}
 	
@@ -24,15 +22,11 @@
 	ChromeWindow *frontmostWindow = nil;
 	SBElementArray *windows = [chrome windows];
 	if (windows == nil) {
-    NSLog(@"[raindrop:chrome] error getting windows");
 		return nil;
 	}
   
-  NSLog(@"[raindrop:chrome] chrome windows: %@ - %d", windows, windows.count);
-	
 	for (ChromeWindow *window in windows) {
-    NSLog(@"[raindrop:chrome] window: %@", window);
-		if (frontmostWindow == nil) {
+  	if (frontmostWindow == nil) {
 			frontmostWindow = window;
 		} else if (frontmostWindow.index > window.index) {
 			frontmostWindow = window;
@@ -40,19 +34,15 @@
 	}
 	
 	if (frontmostWindow == nil) {
-    NSLog(@"[raindrop:chrome] error, no frontmost window");
 		return nil;
 	}
 	
 	// Get active tab
 	ChromeTab *tab = [frontmostWindow activeTab];
 	if (tab == nil || tab.URL == nil) {
-    NSLog(@"[raindrop:chrome] error getting active tab");
 		return nil;
 	}
-	
-  NSLog(@"[raindrop:chrome] using tab: %@ - %@", tab.title, tab.URL);
-  
+	  
 	// Write to pasteboard
 	NSPasteboardItem *item = [[NSPasteboardItem alloc] init];
 	[item setString:tab.title != nil ? tab.title : tab.URL forType:@"public.url-name"];
